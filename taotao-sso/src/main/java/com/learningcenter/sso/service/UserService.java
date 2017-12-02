c417940cf56dc2ada6742ad9e6a87729e3175b72
Jason <cindy851217@gmail.com>
2017-12-02 11:19:11 +0800

    taotao-sso

---
+++ taotao-sso/src/main/java/com/learningcenter/sso/service/UserService.java
@@ -0,0 +1,15 @@
+package com.learningcenter.sso.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.learningcenter.common.pojo.TaotaoResult;
+import com.learningcenter.pojo.TbUser;
+
+public interface UserService {
+
+	TaotaoResult checkData(String content, Integer type);
+	TaotaoResult createUser(TbUser user);
+	TaotaoResult userLogin(String username, String password, HttpServletRequest request, HttpServletResponse response);
+	TaotaoResult getUserByToken(String token);
+}
